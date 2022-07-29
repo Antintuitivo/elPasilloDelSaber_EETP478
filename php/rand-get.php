b197457626cc490263db69d1e2a930e0e78a4453
PC-6 Aula 7 Informática EETP 478 <tomax132@gmail.com>
2022-07-29 19:12:09 -0300

    organizing cvs reading

--- php/rand-get.php
+++ php/rand-get.php
@@ -3,6 +3,11 @@ $age = $_POST['edad'];
 $min = 1;
 $max = 90;
 $question_index = rand($min,$max);
+$file = "file.csv";
+$length = 1000;
+$separator = ";";
 
+$resource= fopen($file,"r");
+$preguntas[] = fgetcsv($resource, $length, $separator);
 
 ?>
