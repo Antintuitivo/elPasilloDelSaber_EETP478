72bb4dca53d536d18c610a561dead6d6a491d002
PC-6 Aula 7 Informática EETP 478 <tomax132@gmail.com>
2022-08-01 21:16:15 -0300

    getcsv
    trying to get the csv file into a multidimensional array

--- php/rand-get.php
+++ php/rand-get.php
@@ -1,13 +1,21 @@
 <?php 
-$age = $_POST['edad'];
-$min = 1;
-$max = 90;
-$question_index = rand($min,$max);
-$file = "file.csv";
-$length = 1000;
-$separator = ";";
+$age = $_POST['edad'];//save the user's age to accurateley
+$min = 0;//minimum number of the question index to select
+$max = 3;//90//maximum number of the question index to select
+$question_index = rand($min,$max);//randomize the number of the question to fetch
+$file = "file.csv";//dirección del archivo csv
+$length = 1000;//max length of characters to read in the csv file
+$separator = ';';//separator used inside the csv file to separate values
 
-$resource= fopen($file,"r");
-$preguntas[] = fgetcsv($resource, $length, $separator);
+$resource= fopen($file,"r");//return information needed by fgetcsv()
 
+//$get_questions = fgetcsv($resource, $length, $separator))
+    //$present = var_dump($get_questions);    //get array of questions from the file
+    
+    $csv = array_map('fgetcsv()', file('file.csv'));
+    echo '<pre>';
+    print_r($csv[$question_index]);
+    print_r($csv);
+    echo '</pre>';
+    echo $question_index
 ?>
