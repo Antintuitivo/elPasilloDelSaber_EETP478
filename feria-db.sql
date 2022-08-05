f2b053c82f811f00ef7eb4c96649f2e25b2496d4
Juan-Ignacio-Bianchini <juanignaciobianchini@gmail.com>
2022-08-05 18:01:41 -0300

    Update feria-db.sql

--- feria-db.sql
+++ feria-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-08-2022 a las 20:45:23
+-- Tiempo de generación: 05-08-2022 a las 22:43:45
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -24,33 +24,47 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `ranking`
+-- Estructura de tabla para la tabla `journey`
 --
 
-CREATE TABLE `ranking` (
-  `id-position` int(11) NOT NULL,
-  `position-nick` varchar(3) COLLATE utf8_spanish_ci NOT NULL COMMENT 'Nick ingresado por el usuario.',
-  `position-score` int(11) NOT NULL COMMENT 'Puntaje total en el juego.',
-  `position-et` time NOT NULL COMMENT 'Tiempo transcurrido (elapsed time, et) total en el juego.'
+CREATE TABLE `journey` (
+  `id-user` int(11) NOT NULL COMMENT 'Código de identificación único para cada usuario.',
+  `journey-started` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Hora de iniciado el recorrido.',
+  `journey-step` int(1) NOT NULL DEFAULT 0 COMMENT 'Punto del trayecto en el circuito de preguntas (0-9).',
+  `journey-stage` enum('0','1','2','3') COLLATE utf8_spanish_ci NOT NULL DEFAULT '0' COMMENT 'Número de etapa del circuito en donde se encuentra el usuario.',
+  `journey-ended` datetime DEFAULT NULL COMMENT 'Hora de finalizado el recorrido.'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `sensors`
+-- Estructura de tabla para la tabla `ranking`
 --
 
-CREATE TABLE `sensors` (
+CREATE TABLE `ranking` (
+  `id-user` int(11) NOT NULL COMMENT 'Código de identificación único para cada usuario.',
+  `ranking-nick` varchar(3) COLLATE utf8_spanish_ci NOT NULL COMMENT 'Nick ingresado por el usuario.',
+  `ranking-score` int(11) NOT NULL COMMENT 'Puntaje total en el juego.',
+  `ranking-et` time NOT NULL COMMENT 'Tiempo transcurrido (elapsed time, et) total en el juego.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `signals`
+--
+
+CREATE TABLE `signals` (
   `id-record` int(11) NOT NULL COMMENT 'Código de registro único.',
-  `sensors-stage` enum('1','2','3') COLLATE utf8_spanish_ci NOT NULL COMMENT 'Número de sensor o etapa del circuito en donde se registro actividad.',
-  `sensors-time` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Fecha y hora de la creación del registro.'
+  `signal-stage` enum('1','2','3') COLLATE utf8_spanish_ci NOT NULL COMMENT 'Número de sensor o etapa del circuito en donde se registro actividad.',
+  `signal-time` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Fecha y hora de la creación del registro.'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `sensors`
+-- Volcado de datos para la tabla `signals`
 --
 
-INSERT INTO `sensors` (`id-record`, `sensors-stage`, `sensors-time`) VALUES
+INSERT INTO `signals` (`id-record`, `signal-stage`, `signal-time`) VALUES
 (1, '3', '2022-08-05 14:47:38'),
 (2, '3', '2022-08-05 15:11:23'),
 (3, '3', '2022-08-05 15:11:32'),
@@ -73,35 +87,39 @@ INSERT INTO `sensors` (`id-record`, `sensors-stage`, `sensors-time`) VALUES
 
 CREATE TABLE `users` (
   `id-user` int(11) NOT NULL COMMENT 'Código de identificación único para cada usuario.',
-  `user-nick` varchar(3) COLLATE utf8_spanish_ci NOT NULL COMMENT 'Nick ingresado por el usuario.',
-  `user-email` varchar(30) COLLATE utf8_spanish_ci NOT NULL COMMENT 'Dirección de correo electrónico del individuo.',
-  `user-age` int(3) NOT NULL COMMENT 'Edad del individuo.',
-  `user-created` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Hora de creación del usuario.',
-  `user-journey` enum('0','1','2','3','4','5','6','7','8','9') COLLATE utf8_spanish_ci NOT NULL DEFAULT '0' COMMENT 'Punto del trayecto en el circuito de preguntas (0-9).'
+  `user-email` varchar(30) COLLATE utf8_spanish_ci NOT NULL COMMENT 'Dirección de correo electrónico del usuario.',
+  `user-age` int(3) NOT NULL COMMENT 'Edad del usuario.'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id-user`, `user-nick`, `user-email`, `user-age`, `user-created`, `user-journey`) VALUES
-(1, '', 'juanignaciobianchini@gmail.com', 19, '2022-07-27 22:25:35', '0'),
-(2, '', 'f@gmail.com', 20, '2022-07-27 22:33:52', '0');
+INSERT INTO `users` (`id-user`, `user-email`, `user-age`) VALUES
+(1, 'juanignaciobianchini@gmail.com', 19),
+(2, 'f@gmail.com', 20);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `journey`
+--
+ALTER TABLE `journey`
+  ADD UNIQUE KEY `id-user` (`id-user`);
+
+--
 -- Indices de la tabla `ranking`
 --
 ALTER TABLE `ranking`
-  ADD UNIQUE KEY `position-nick` (`position-nick`);
+  ADD UNIQUE KEY `position-nick` (`ranking-nick`),
+  ADD UNIQUE KEY `id-user` (`id-user`);
 
 --
--- Indices de la tabla `sensors`
+-- Indices de la tabla `signals`
 --
-ALTER TABLE `sensors`
+ALTER TABLE `signals`
   ADD UNIQUE KEY `id-record` (`id-record`);
 
 --
@@ -116,9 +134,9 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT de la tabla `sensors`
+-- AUTO_INCREMENT de la tabla `signals`
 --
-ALTER TABLE `sensors`
+ALTER TABLE `signals`
   MODIFY `id-record` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Código de registro único.', AUTO_INCREMENT=14;
 
 --
@@ -126,6 +144,22 @@ ALTER TABLE `sensors`
 --
 ALTER TABLE `users`
   MODIFY `id-user` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Código de identificación único para cada usuario.', AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `journey`
+--
+ALTER TABLE `journey`
+  ADD CONSTRAINT `journey_ibfk_1` FOREIGN KEY (`id-user`) REFERENCES `users` (`id-user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `ranking`
+--
+ALTER TABLE `ranking`
+  ADD CONSTRAINT `ranking_ibfk_1` FOREIGN KEY (`id-user`) REFERENCES `users` (`id-user`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
