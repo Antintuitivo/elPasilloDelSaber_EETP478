362e99c73314b80884699141f143edfea807bb6b
Juan-Ignacio-Bianchini <juanignaciobianchini@gmail.com>
2022-08-14 15:49:00 -0300

    Update raspberry scripts.

--- shell/test.sh
+++ shell/test.sh
@@ -1,19 +1,19 @@
 while :; do
-read texto;
-#Add a delay
-if [ $texto  = 1 ]; then
-sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO sensors (`sensors-stage`) VALUES ('1')' feria-db;
-echo "fffffffff";
-sleep 4;
-fi
-if [ $texto  = 2 ]; then
-sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO sensors (`sensors-stage`) VALUES ('2')' feria-db;
-echo "fffffffff";
-sleep 4;
-fi
-if [ $texto  = 3 ]; then
-sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO sensors (`sensors-stage`) VALUES ('3')' feria-db;
-echo "fffffffff";
-sleep 4;
-fi
+    read texto;
+    #Add a delay
+    if [ $texto  = 1 ]; then
+        sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO sensors (`sensors-stage`) VALUES ('1')' feria-db;
+        echo "fffffffff";
+        sleep 4;
+    fi
+    if [ $texto  = 2 ]; then
+        sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO sensors (`sensors-stage`) VALUES ('2')' feria-db;
+        echo "fffffffff";
+        sleep 4;
+    fi
+    if [ $texto  = 3 ]; then
+        sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO sensors (`sensors-stage`) VALUES ('3')' feria-db;
+        echo "fffffffff";
+        sleep 4;
+    fi
 done
