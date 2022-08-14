a8839fb6d2090d203ff36e136474dd83fe4653d0
Juan-Ignacio-Bianchini <juanignaciobianchini@gmail.com>
2022-08-13 23:24:14 -0300

    Add raspberry scripts.

---
+++ shell/raspberry-daemon.sh
@@ -0,0 +1,71 @@
+#echo $! > ./raspberry-daemon.pid; #Registro del Process ID, para finalizarlo.
+#sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'SELECT * FROM signals ORDER BY `id-record` DESC LIMIT 1' feria-db;
+echo "Welcome to the input-output control program.";
+echo -e "Write 1 for Raspberry or 1 for terminal-only tests:";
+read mode;
+if [ $mode = 1]; then
+#Establecer pines GPIO como receptores (escucha).
+gpio mode 2 in; #Sensor/Interruptor etapa 1
+gpio mode 21 in; #Sensor/Interruptor etapa 2
+gpio mode 23 in; #Sensor/Interruptor etapa 3
+
+#Establecer pines GPIO como emisores (salida).
+gpio mode 3 out; #LED etapa 1
+gpio mode 22 out; #LED etapa 2
+gpio mode 24 out; #LED etapa 3
+
+#Inicializar varaiables.
+lastsensor = 0;
+
+while :; do
+if [ "$(gpio read 2)" -eq 0 ] && [ "$lastsensor" -ne 1 ]; then
+sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO signals (`signal-stage`) VALUES ('1')' feria-db;
+lastsensor = 1;
+echo "Llegada a etapa 1 registrada";
+gpio write 3 1;
+sleep 10;
+gpio write 3 0;
+fi
+if [ "$(gpio read 21)" -eq 0 ] && [ "$lastsensor" -ne 2 ]; then
+sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO signals (`signal-stage`) VALUES ('2')' feria-db;
+lastsensor = 2;
+echo "Llegada a etapa 2 registrada";
+gpio write 22 1;
+sleep 10;
+gpio write 22 0;
+fi
+if [ "$(gpio read 23)" -eq 0 ] && [ "$lastsensor" -ne 3 ]; then
+sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO signals (`signal-stage`) VALUES ('3')' feria-db;
+lastsensor = 3;
+echo "Llegada a etapa 3 registrada";
+gpio write 24 1;
+sleep 10;
+gpio write 24 0;
+fi
+done
+fi
+
+if [ $mode = 2 ]; then
+lastsensor = 0;
+while :; do
+read signal;
+if [ "$signal" -eq 1 ] && [ "$lastsensor" -ne 1 ]; then
+sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO signals (`signal-stage`) VALUES ('1')' feria-db;
+lastsensor = 1;
+echo "Llegada a etapa 1 registrada";
+sleep 10;
+fi
+if [ "$signal" -eq 2 ] && [ "$lastsensor" -ne 2 ]; then
+sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO signals (`signal-stage`) VALUES ('2')' feria-db;
+lastsensor = 2;
+echo "Llegada a etapa 2 registrada";
+sleep 10;
+fi
+if [ "$signal" -eq 3 ] && [ "$lastsensor" -ne 3 ]; then
+sudo mysql -h 10.0.1.40 -u raspberry -p'-r4spb3rry-' -e 'INSERT INTO signals (`signal-stage`) VALUES ('3')' feria-db;
+lastsensor = 3;
+echo "Llegada a etapa 3 registrada";
+sleep 10;
+fi
+done
+fi
