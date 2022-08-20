d7eca52d648c565d931af0bb81b53c6791884bc0
Juan-Ignacio-Bianchini <juanignaciobianchini@gmail.com>
2022-08-20 00:50:51 -0300

    Add basic CSS structure.

--- php/intermedio-page.php
+++ php/intermedio-page.php
@@ -1,4 +1,11 @@
-<h1>Ve a la siguiente etapa</h1>
-<div><script src="../../web/javascript/change-stage.js"></script></div>
-<h1>It's working!</h1>
-<img src="//media0.giphy.com/media/CuMiNoTRz2bYc/giphy.gif">
+<head>
+    <link rel="stylesheet" type="text/css" href="../../web/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="../../web/css/messages.css">
+    <link rel="stylesheet" type="text/css" href="../../web/css/intermedio.css">
+</head>
+<body>
+    <h1>Ve a la siguiente etapa</h1>
+    <div><script src="../../web/javascript/change-stage.js"></script></div>
+    <h1>It's working!</h1>
+    <img src="//media0.giphy.com/media/CuMiNoTRz2bYc/giphy.gif">
+</body>
