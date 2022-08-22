151a17d4f7dad9ef697f1e54073ca769703036c2
Juan-Ignacio-Bianchini <juanignaciobianchini@gmail.com>
2022-08-22 20:23:05 -0300

    Update nick.php

--- php/nick.php
+++ php/nick.php
@@ -1,16 +1,35 @@
 <?php
   session_start();
 
+  #Inicializar variables a utilizar.
+  $id = $_SESSION['usuario']['id'];
+  $score = $_SESSION['tabla']['puntaje'];
+
   #Obtener variables desde el formulario en HTML, con el método POST.
   $nick = $_POST['nick'];
-  $id = $_SESSION['usuario']['id'];
-  $id = $_SESSION['usuario']['id'];
-
+  
   #Realizar, comprobar y almacenar credecenciales de la conexión a la DBMS.
   #-----------------------------------------------------------------------------
   include 'header\link.php';
   $link = connect();
 
+  #Recuperación y comparación del tiempo transcurrido en el juego.
+  #-----------------------------------------------------------------------------
+  $select = "SELECT*FROM journey WHERE `id-user`='$id'";
+  $result = mysqli_query($link, $select);
+  $validation = mysqli_num_rows($result);
+
+  $journey = mysqli_fetch_assoc($result);
+
+  $first  = date_create($journey['journey-started']);
+  $second = date_create($journey['journey-ended']);
+
+  //echo date_format($first, 'H:i:s') . "<br>";
+  //echo date_format($second, 'H:i:s') . "<br>";
+
+  $diff = $first->diff($second);
+  $et = $diff->format( '%H:%I:%S' );
+#---------------------------------------------------------------------------------------------
   #Comprobar si el nick está utilizado.
   #-----------------------------------------------------------------------------
   $select = "SELECT*FROM ranking WHERE `ranking-nick`='$nick'";
@@ -20,7 +39,7 @@
   echo "<h1>" . $validation . "<h1>";
   if ($validation == 0) {
     #Insertar entrada a la tabla.
-    $insert = "INSERT INTO ranking (`id-user`, `ranking-nick`, `ranking-score`, `ranking-et`) VALUES ('$id', '$nick')";
+    $insert = "INSERT INTO ranking (`id-user`, `ranking-nick`, `ranking-score`, `ranking-et`) VALUES ('$id', '$nick', '$score', '$et')";
     mysqli_query($link, $insert);
     $result = mysqli_query($link, $select);
   } else{
