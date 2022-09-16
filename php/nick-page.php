c18af7314a3709d884c8871cf67580dcdb4b8128
PC-6 Aula 7 Informática EETP 478 <tomax132@gmail.com>
2022-09-15 21:16:35 -0300

    avance css
    falta adaptar páginas nick y ranking

--- php/nick-page.php
+++ php/nick-page.php
@@ -19,8 +19,8 @@ if (isset($_SESSION['nick_message'])) {
     <title>El pasillo del saber</title>
     <link rel="icon" href="img\school-icon.svg">
     <link rel="stylesheet" type="text/css" href="../../web/css/normalize.css">
-    <link rel="stylesheet" type="text/css" href="../../web/css/messages.css">
     <link rel="stylesheet" type="text/css" href="../../web/css/nick.css">
+    <link rel="stylesheet" type="text/css" href="../../web/css/messages.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Aboreto&display=swap" rel="stylesheet">
@@ -30,11 +30,12 @@ if (isset($_SESSION['nick_message'])) {
     <div class="transparent-background"></div>
     <div class="big-box">
         <form method="POST" class="contenedor">
+            <img src="../../web/img/school-icon.svg">
             <h2>Ingresar nick</h2>
             <input class="input-100" type="text" placeholder="Ingresar nick" name="nick" maxlength="3" required>
             <div class="stats">
-                <span class="puntos">Puntos: <?PHP echo $_SESSION['tabla']['puntaje'];?></span>
-                <span class="puntos">Tiempo: <?PHP echo $_SESSION['tabla']['tiempo'];?></span>
+                <span class="puntos"><b>Puntos</b>: <?PHP echo $_SESSION['tabla']['puntaje'];?></span>
+                <span class="puntos"><b>Tiempo</b>: <?PHP echo $_SESSION['tabla']['tiempo'];?></span>
             </div>
             <input class="input-100 btn-enviar" type="submit" value="INGRESAR" formaction="../../web/php/nick.php">
         </form>
