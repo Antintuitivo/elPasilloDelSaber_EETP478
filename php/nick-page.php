1925f9020052d68c2ef8d4401f0c246911da059a
PC-6 Aula 7 Informática EETP 478 <tomax132@gmail.com>
2022-09-16 18:10:13 -0300

    adaptabilidad
    hasta que se vea algún problema en la adaptabilidad, se consideran terminadas las modificaciones de este tipo

--- php/nick-page.php
+++ php/nick-page.php
@@ -31,8 +31,8 @@ if (isset($_SESSION['nick_message'])) {
             <h2>Ingresar nick</h2>
             <input class="input-100" type="text" placeholder="Ingresar nick" name="nick" maxlength="3" required>
             <div class="stats">
-                <span class="puntos"><b>Puntos</b>: <?PHP echo $_SESSION['tabla']['puntaje'];?></span>
-                <span class="puntos"><b>Tiempo</b>: <?PHP echo $_SESSION['tabla']['tiempo'];?></span>
+                <span class="puntos"><b>Puntos:</b> <?PHP echo $_SESSION['tabla']['puntaje'];?></span>
+                <span class="puntos"><b>Tiempo:</b> <?PHP echo $_SESSION['tabla']['tiempo'];?></span>
             </div>
             <input class="input-100 btn-enviar" type="submit" value="INGRESAR" formaction="../../web/php/nick.php">
         </form>
