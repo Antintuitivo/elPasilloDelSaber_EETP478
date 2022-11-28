cf77af5b58e24eec14de5119cd08360e3f3ff309
Juan-Ignacio-Bianchini <juanignaciobianchini@gmail.com>
2022-11-28 16:45:01 -0300

    Delete unnesesary files

--- php/header/closer.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    session_unset();
-    session_destroy();
-    setcookie("PHPSESSID", "", time()-1000,"/", "127.0.0.1",false,false);
-    header("Location: /../web/php/ranking.php");
-?>
